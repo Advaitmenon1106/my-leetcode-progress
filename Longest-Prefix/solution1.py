957768e127b22628e098ae75b04350c5b7b483cf
advaitmenon1106 <advaitmenon1106@gmail.com>
2024-08-10 20:17:06 +0530

    Add the solution to the longest prefix problem.

---
+++ Longest-Prefix/solution1.py
@@ -0,0 +1,22 @@
+
+def longestCommonPrefix(self, strs: List[str]) -> str:
+    pref = ''
+    if len(strs) == 0:
+        return ''
+    elif len(strs) == 1:
+        return strs[0]
+    smallestWord = sorted(strs, key=len)[0]
+    count = 0
+    word = ''
+    for i in range(len(smallestWord)):
+        for word in sorted(strs, key=len)[1:]:
+            if word[i] == smallestWord[i]:
+                count+=1
+            else:
+                break
+        if word[i]!=smallestWord[i]: 
+            break
+        if count == len(strs)-1:
+            pref+=smallestWord[i]
+        count = 0
+    return pref
