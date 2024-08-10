9026203e078e3486bce675e9b741c8bb865d0bfa
advaitmenon1106 <advaitmenon1106@gmail.com>
2024-08-10 20:44:49 +0530

    Remove the self parameter which accidentally got added in the code.

--- Palindrome-Number/solution1.py
+++ Palindrome-Number/solution1.py
@@ -1,4 +1,4 @@
-def isPalindrome(self, x: int) -> bool:
+def isPalindrome(x: int) -> bool:
     stringOfNumber = list(str(x))
     reverse = stringOfNumber[::-1]
     return reverse == stringOfNumber
